c740b9be5142589a50c98325d99edee5e679d500
Thalia Nero <chrisvn00@gmail.com>
2024-08-23 20:20:53 -0500

    Add smoke test for expr.

--- src/parse.rs
+++ src/parse.rs
@@ -69,3 +69,55 @@ macro_rules! declare_idents {
 }
 #[cfg(test)]
 pub(crate) use declare_idents;
+
+#[cfg(test)]
+mod tests {
+    use crate::{cache::{StringCache, StringKey}, diagnostic::Diagnostics, tokenizer::Tokenizer, token::TokenKind};
+
+    use super::Parser;
+
+    #[test]
+    fn valid_expressions_smoke() {
+        let exprs = [
+            "1",
+            "a",
+            "a.b",
+            "a.b.c",
+            "a::b",
+            "a::b.c",
+            "f()",
+            "a.f()",
+            "a::b.f()",
+            "a.f().b",
+            "f(1)",
+            "f(1, 2)",
+            "1 + 2",
+            "1 + 2 + 3",
+            "(1 + 2) * 3",
+            "f(1 + 2)",
+            "{}",
+            "{ a }",
+            "{ a; }",
+            "{ a + b }",
+            "{ {} }",
+            "{ {}; }",
+            "if 1 { 2 } else { 3 }",
+            "do { 1 }",
+            "{ x: Int, y: Int -> x + y }",
+        ];
+
+        for input in exprs {
+            let mut cache = StringCache::new();
+            let mut ds = Diagnostics::new();
+            let tz = Tokenizer::from_parts(StringKey::EMPTY, input);
+            let mut parser = Parser {
+                tz,
+                cache: &mut cache,
+                ds: &mut ds,
+            };
+            let _ = parser.block_expr();
+            assert_eq!(TokenKind::Eof, *parser.tz.next());
+            assert!(!parser.ds.has_errors());
+        }
+    }
+}
